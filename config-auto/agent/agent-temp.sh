17fdb4db0c7a22613618e74005d2339e75840eb0
moran233 <9843841+moran2331064@user.noreply.gitee.com>
2024-06-30 10:52:34 +0800

    2

--- config-auto/agent/agent-temp.sh
+++ config-auto/agent/agent-temp.sh
@@ -37,11 +37,11 @@ else
     echo "换源失败，请手动换源！"
 fi
 
-sudo apt-get update
+sudo apt-get -y update
 sudo apt-get upgrade -y --no-install-recommends -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
 sudo apt-get -y --no-install-recommends install apt-transport-https ca-certificates curl software-properties-common dig
 curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository -y "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get -y update
 sudo apt-get -y --no-install-recommends install docker-ce
 
